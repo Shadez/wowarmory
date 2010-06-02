f11878bd1853e6854f4f979e4badff35766256af
Shadez <d.master.me@gmail.com>
2010-06-02 22:25:41 +0900

    [223] Fixed `armory_instance_data` structure

---
+++ sql/updates/armory_r223_armory_instance_data.sql
@@ -0,0 +1,7 @@
+UPDATE `armory_db_version` SET `version` = 'armory_r223';
+ALTER TABLE `armory_instance_data` CHANGE `name_es_es` `name_de_de_tmp` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `armory_instance_data` CHANGE `name_ru_ru` `name_es_es_tmp` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `armory_instance_data` CHANGE `name_de_de` `name_ru_ru_tmp` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci;
+ALTER TABLE `armory_instance_data` CHANGE `name_de_de_tmp` `name_de_de` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL;
+ALTER TABLE `armory_instance_data` CHANGE `name_ru_ru_tmp` `name_ru_ru` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL;
+ALTER TABLE `armory_instance_data` CHANGE `name_es_es_tmp` `name_es_es` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL;
