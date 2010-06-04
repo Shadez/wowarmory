86dd20409eb016c3742dae14210b3277541da7fa
Shadez <d.master.me@gmail.com>
2010-06-04 11:52:51 +0900

    [227] Missed strings for Draenei race (thanks to heugabel @ getmangos.com)

--- includes/armory_loader.php
+++ includes/armory_loader.php
@@ -3,7 +3,7 @@
 /**
  * @package World of Warcraft Armory
  * @version Release Candidate 1
- * @revision 223
+ * @revision 227
  * @copyright (c) 2009-2010 Shadez
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
@@ -30,8 +30,8 @@ error_reporting(E_ALL);
 if(!@include('classes/class.connector.php')) {
     die('<b>Error:</b> can not load connector class!');
 }
-define('DB_VERSION', 'armory_r223');
-define('ARMORY_REVISION', 223);
+define('DB_VERSION', 'armory_r227');
+define('ARMORY_REVISION', 227);
 $armory = new Connector();
 /* Check DbVersion */
 $dbVersion = $armory->aDB->selectCell("SELECT `version` FROM `armory_db_version`");
---
+++ sql/updates/armory_r227_armory_races.sql
@@ -0,0 +1,9 @@
+/*!40101 SET NAMES utf8 */;
+/*!40101 SET SQL_MODE=''*/;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+UPDATE `armory_db_version` SET `version`='armory_r227';
+INSERT INTO `armory_races` (`id`, `name_de_de`, `name_en_gb`, `name_es_es`, `name_fr_fr`, `name_ru_ru`, `modeldata_1`, `modeldata_2`) VALUES
+(11, 'Draenei', 'Draenei', 'Draenei', 'Draenei', 'Draenei', 'Дренеи', 'dr');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
