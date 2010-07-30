36949b24995b2cf47a697e59b16f03e95449d05b
Shadez <d.master.me@gmail.com>
2010-07-31 05:28:04 +0900

    [336] Set correct place for some player titles (suffix)

--- includes/revision_nr.php
+++ includes/revision_nr.php
@@ -1,5 +1,5 @@
 <?php
-define('ARMORY_REVISION', 335);
-define('DB_VERSION', 'armory_r307');
+define('ARMORY_REVISION', 336);
+define('DB_VERSION', 'armory_r336');
 define('CONFIG_VERSION', 1107201001);
 ?>
---
+++ sql/updates/armory_r336_armory_titles.sql
@@ -0,0 +1,2 @@
+UPDATE `armory_db_version` SET `version` = 'armory_r336';
+UPDATE `armory_titles` SET `place` = 'suffix' WHERE `id` IN (36, 38, 60, 86, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121);
