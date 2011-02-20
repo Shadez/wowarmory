5d07d76a8ad3d091888963ff5ed98e40026bda2a
Shadez <d.master.me@gmail.com>
2011-02-20 15:28:01 +0800

    [485] Display faction icon on arena team page.

--- includes/classes/class.arenateams.php
+++ includes/classes/class.arenateams.php
@@ -3,7 +3,7 @@
 /**
  * @package World of Warcraft Armory
  * @version Release 4.50
- * @revision 456
+ * @revision 485
  * @copyright (c) 2009-2011 Shadez
  * @license http://opensource.org/licenses/gpl-license.php GNU Public License
  *
@@ -122,6 +122,7 @@ Class Arenateams {
         else {
             $arenateaminfo['members'] = self::GetTeamList();
         }
+        $arenateaminfo['data']['factionId'] = Utils::GetFactionId($arenateaminfo['members'][0]['raceId']);
         return $arenateaminfo;
     }
     
--- includes/revision_nr.php
+++ includes/revision_nr.php
@@ -1,5 +1,5 @@
 <?php
-define('ARMORY_REVISION', 484);
+define('ARMORY_REVISION', 485);
 define('DB_VERSION', 'armory_r471');
 define('CONFIG_VERSION', '2001201101');
 ?>
