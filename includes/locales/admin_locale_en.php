7a4df8b81438e3028224461600250a64c03fe84f
Shadez <d.master.me@gmail.com>
2011-03-20 00:53:28 +0800

    [491] Locale file for admin panel (not used now)

---
+++ includes/locales/admin_locale_en.php
@@ -0,0 +1,38 @@
+<?php
+
+/**
+ * @package World of Warcraft Armory
+ * @version Release 4.50
+ * @revision 467
+ * @copyright (c) 2009-2011 Shadez
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ **/
+
+if(!defined('__ARMORY__')) {
+    die('Direct access to this file not allowed!');
+}
+
+$ArmoryAdminLocale = array(
+    'lang_name' => 'English',
+    'lang_id' => 0,
+    'lang_locale' => 'en',
+    
+    'template_welcome_header' => 'Welcome, ',
+    'template_logout' => 'Logout',
+    
+);
+?>
--- includes/revision_nr.php
+++ includes/revision_nr.php
@@ -1,5 +1,5 @@
 <?php
-define('ARMORY_REVISION', 490);
+define('ARMORY_REVISION', 491);
 define('DB_VERSION', 'armory_r489');
 define('CONFIG_VERSION', '2001201101');
 ?>
